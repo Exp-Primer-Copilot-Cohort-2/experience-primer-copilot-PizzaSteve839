79b8af59551c2891a6127fbb6428e4de27730488
PizzaSteve839 <100524757@alumnos.uc3m.es>
2024-09-10 20:04:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,5 @@
-//Create a web server that's going to send a response of big image (bigger then 3MB) to any client that sends a request to your specified server:port. Use the best way for performance.
-
-var http = require('http');
-var fs = require('fs');
-
-http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'image/jpg'});
-
-  var img = fs.readFileSync(__dirname + '/big.jpg');
-  res.end(img, 'binary');
-}).listen(3000);
-
-console.log('Server is running at http://localhost:3000/');
+// Create web server and listen on port 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const comments = require('./comments');
+const app =
